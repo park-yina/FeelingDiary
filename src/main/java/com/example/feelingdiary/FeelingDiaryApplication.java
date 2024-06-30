5c29e54c5b9c09e600b831b6285acd384e622f6d
park-yina <kln99988@gmail.com>
2024-06-30 13:15:29 +0900

    기본세팅

---
+++ src/main/java/com/example/feelingdiary/FeelingDiaryApplication.java
@@ -0,0 +1,13 @@
+package com.example.feelingdiary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeelingDiaryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FeelingDiaryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/feelingdiary/FeelingDiaryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.feelingdiary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeelingDiaryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
